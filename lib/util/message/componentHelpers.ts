d61fed5af508d5070fda0f72c1c100e06150d541
Samuel Voeller <12182390+samuel-voeller@users.noreply.github.com>
2025-10-05 19:24:56 +0000

    fix: correct double message on fastGenericComponentV2

--- lib/util/message/componentHelpers.ts
+++ lib/util/message/componentHelpers.ts
@@ -57,7 +57,6 @@ export function fastGenericComponentV2(
     builder.addSeparatorComponents((b) => b.setSpacing(Discordeno.SeparatorSpacingSize.Small));
   }
   builder
-    .addTextDisplayComponents((b) => b.setContent(parts.content ?? 'No content provided'))
     .addSeparatorComponents((b) => b.setSpacing(Discordeno.SeparatorSpacingSize.Small))
     .addTextDisplayComponents((b) => b.setContent(`-# <t:${Math.floor(Date.now() / 1000)}:F>`));
 
